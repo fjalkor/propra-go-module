fe3627f0709976d2e0d4123cd0d412718b6c3d2b
Ihar Brandes <ihar.brandes@check24.de>
2025-07-06 20:57:41 +0200

    add v2

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/fjalkor/propra-go-module/v2
+
+go 1.23.3
--- v2/main.go
+++ v2/main.go
@@ -3,5 +3,5 @@ package propragomodule
 import "fmt"
 
 func HiFromRemote() {
-    fmt.Println("Hi from remote module v2!")
+    fmt.Println("Hi from remote module!")
 }
